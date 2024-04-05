04fcf252fe0c4140e7ddb7d77dbabc13ba3ed611
Craig Clayton <craig__clayton@outlook.com>
2024-04-05 14:44:21 +0100

    basic post request working in Postman

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
+app.use(express.json());
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -12,3 +13,33 @@ const blogSchema = new mongoose.Schema({
 });
 
 const Blog = mongoose.model('Blog', blogSchema);
+
+const mongoUrl =
+  'mongodb+srv://craigclayton:Blakemon1@cluster0.xpxvblq.mongodb.net/Blog?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoUrl);
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs);
+  });
+});
+
+app.post('/api/blogs', (request, response) => {
+  const { title, author, url, likes } = request.body;
+
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes,
+  });
+
+  blog.save().then(savedBlog => {
+    response.status(201).json(savedBlog);
+  });
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
