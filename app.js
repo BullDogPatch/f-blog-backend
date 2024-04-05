98d2dbaf2221d76142e4d1110b3acd765d3f1f1e
Craig Clayton <craig__clayton@outlook.com>
2024-04-05 17:34:05 +0100

    add dist folder for front end

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose.connect(config.MONGODB_URL).then(() => {
 });
 
 // app.use(cors());
-// app.use(express.static('dist'));
+app.use(express.static('dist'));
 app.use(express.json());
 // app.use(middleware.requestLogger);
 
