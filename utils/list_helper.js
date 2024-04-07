d10f7284ee68cd47e7694545d6f50b22833bea3a
Craig Clayton <craig__clayton@outlook.com>
2024-04-07 16:36:38 +0100

    add some tests, install lodash

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -14,10 +14,48 @@ const favoriteBlog = blogs => {
   return blogs.sort((a, b) => b.likes - a.likes);
 };
 
-const mostBlogs = blogs => {};
+const mostBlogs = blogs => {
+  const blogCount = {};
+
+  blogs.forEach(blog => {
+    if (blog.author in blogCount) {
+      return blogCount[blog.author]++;
+    } else {
+      blogCount[blog.author] = 1;
+    }
+  });
+
+  let maxBlogs = 0;
+  let maxBlogsAuthor = '';
+  for (const author in blogCount) {
+    if (blogCount[author] > maxBlogs) {
+      maxBlogs = blogCount[author];
+      maxBlogsAuthor = author;
+    }
+  }
+
+  return {
+    author: maxBlogsAuthor,
+    blogs: maxBlogs,
+  };
+};
+
+const mostBlogsWithLodash = blogs => {
+  const groupedBlogs = _.groupBy(blogs, 'author');
+  const mostBlogsAuthor = _.maxBy(
+    Object.keys(groupedBlogs),
+    author => groupedBlogs[author].length
+  );
+  return {
+    author: mostBlogsAuthor,
+    blogs: groupedBlogs[mostBlogsAuthor].length,
+  };
+};
 
 module.exports = {
   dummy,
   numOfLikes,
   favoriteBlog,
+  mostBlogs,
+  mostBlogsWithLodash,
 };
